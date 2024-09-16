618dc2609507e294c1310892b11eb2a30f029552
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2024-09-16 15:36:21 -0500

    Update main.py
    Signed-off-by: PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>

--- audit_tool/main.py
+++ audit_tool/main.py
@@ -2,8 +2,11 @@
 
 from audit_tool.database.setup_database import setup_database, add_admin_user
 from audit_tool.gui.login_gui import login_gui
+from audit_tool.sn1per.scanner import run_sn1per_scan
 
 if __name__ == "__main__":
     setup_database()
     add_admin_user()
     login_gui()
+    # Example usage of the Sn1per scanner
+    run_sn1per_scan("example_target")
