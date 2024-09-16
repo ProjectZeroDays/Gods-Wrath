7821609f771b36be311da865774d0e047dbac0b0
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2024-09-16 15:23:23 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from audit_tool.database.setup_database import setup_database, add_admin_user
-from audit_tool.gui.login_gui import login_gui
-
-if __name__ == "__main__":
-    setup_database()
-    add_admin_user()
-    login_gui()
